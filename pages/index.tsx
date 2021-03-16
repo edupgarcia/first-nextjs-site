4a4f56440b0ea9bcf307fb8e7df18235271b8dc5
Eduardo Pereira Garcia <edupgarcia.ti@gmail.com>
2021-03-16 09:06:24 -0300

    Added functionality (state management)

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,29 @@
+import React from "react";
+import { useState } from 'react';
+
 function Index() {
   return (
-    <h1>My First Next.js Page</h1>
+    <div>
+      <h1>My First Next.js Page</h1>
+      <Contador />
+    </div>
+    
   );
+}
+
+function Contador() {
+  const [contador, setContador] = useState(1);
+
+  function adicionarContador() {
+    setContador(contador + 1);
+  }
+
+  return (
+    <div>
+    <div>{contador}</div> 
+    <button onClick={adicionarContador}>Adicionar</button>
+    </div>
+  )
 }
 
 export default Index;
